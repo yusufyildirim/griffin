37ef1faa7a6255061dcc365a6cdcd5f1303dfba9
Yusuf YILDIRIM <yusuf@tinythin.gs>
2022-02-05 01:28:14 +0300

    feat: move server code

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -1,2 +1,24 @@
 import express from 'express'
-console.log('Test')
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('MOUNT_COMPONENT', (componentId, props, callback) => {
+    console.log(componentId, props, callback)
+    socket.broadcast.emit('CLIENT_MOUNT_COMPONENT', componentId, props)
+    callback()
+  })
+})
+
+server.listen(5678, () => {
+  console.log('listening on *:5678')
+})
